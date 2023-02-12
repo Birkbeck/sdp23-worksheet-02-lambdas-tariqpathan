200515651fac244b103fec4bd6df47c21ad90ccd
Tariq Pathan <tariq047@gmail.com>
2023-02-12 13:28:41 +0000

    Trying to learn and work with lambdas >:( Implementing String sorting methods using lambdas and a method reference for counting the number of e's

--- src/Outline.java
+++ src/Outline.java
@@ -1,9 +1,46 @@
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class Outline {
   public static void main(String... args) { // varargs alternative to String[]
     Integer[] intArray = {1,7,3,4,8,2};
     System.out.println(Arrays.asList(intArray));
     // Arrays.sort(intArray,.......)
-  } 
+    String[] strings = {"abc", "ab", "teee", "output", "abcde", "anotheeString"};
+    System.out.println("Initial: " + Arrays.asList(strings));
+    ExerciseOne(strings);
+  }
+
+  static void ExerciseOne (String[] strings){
+    Arrays.sort(strings, (s1, s2) -> s1.length() - s2.length());
+    System.out.println("Shortest to longest: " + Arrays.asList(strings));
+
+    Arrays.sort(strings, (s1, s2) -> s2.length() - s1.length());
+    System.out.println("Longest to shortest: " + Arrays.asList(strings));
+
+    Arrays.sort(strings, (s1, s2) -> s1.charAt(0) - s2.charAt(0));
+    System.out.println("By first character: " + Arrays.asList(strings));
+
+    Arrays.sort(strings, (s1, s2) -> eCounter(s2) - eCounter(s1));
+    System.out.println("By e's: " + Arrays.asList(strings));
+
+    Arrays.sort(strings, String::compareTo);
+
+    Arrays.sort(strings, Outline::eChecker);
+    System.out.println("By e's [method reference]: " + Arrays.asList(strings));
+
+  }
+
+  static int eCounter(String s) {
+    int count = 0;
+    for (int i = 0; i < s.length(); i++) {
+      if (s.charAt(i) == 'e') count += 1;
+    }
+    return count;
+  }
+
+  private static int eChecker(String s1, String s2) {
+    return eCounter(s2) - eCounter(s1);
+  }
 }
+
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+        public static void main(String[] args) {
+            Trial num;
+            num = n -> (n + 1000) > 100;
+            System.out.println(num.test(-4));
+        }
+
+
+}
